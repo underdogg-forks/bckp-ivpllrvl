cad57ddf7c2443d67229cf05d5ffc649b84ef797
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2025-09-30 09:58:08 +0200

    Update app/Models/MerchantResponse.php
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/Models/MerchantResponse.php
+++ app/Models/MerchantResponse.php
@@ -9,6 +9,7 @@ class MerchantResponse extends Model
     public $timestamps = false;
 
     protected $table = 'ip_merchant_responses';
+    protected $primaryKey = 'merchant_response_id';
 
     protected $fillable = [
         'invoice_id',
@@ -17,5 +18,10 @@ class MerchantResponse extends Model
         'merchant_response_driver',
         'merchant_response',
         'merchant_response_reference',
+    ];
+    protected $casts = [
+        'invoice_id'                   => 'integer',
+        'merchant_response_successful' => 'boolean',
+        'merchant_response_date'       => 'date',
     ];
 }
